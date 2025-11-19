9c160abe77ee30d817b1f16504e49788c42fb61d
densigohel11 <118363820+densigohel11@users.noreply.github.com>
2025-11-19 17:20:19 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,13 +2,8 @@ variable "region" {
   default = "us-east-1"
 }
 
-variable "ami" {
-  default = "ami-0022a23f36c57e940"  # Amazon Linux AMI
-}
+variable "ami" {}
 
-variable "instance_type" {
-  default = "t4g.micro"
-}
-
+variable "instance_type" {}
 variable "aws-creds" {}
 variable "aws-creds" {}
