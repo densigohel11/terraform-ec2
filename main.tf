7e0887c2f0ad1f3fad4b8b29285bb5142adbd890
densigohel11 <118363820+densigohel11@users.noreply.github.com>
2025-11-19 12:38:34 +0530

    Create  main.tf

---
+++ main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  region     = var.region
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+}
+
+resource "aws_instance" "my_ec2" {
+  ami           = var.ami
+  instance_type = var.instance_type
+
+  tags = {
+    Name = "Terraform-Jenkins-EC2"
+  }
+}
